3027d00b660867d35a3cd00344d6b69eb5cea85d
Konstantina Lazaridou <64789450+Konstantina-Lazaridou@users.noreply.github.com>
2025-06-11 12:47:11 +0200

    feat: first schema.sql with three tables

---
+++ schema.sql
@@ -0,0 +1,3 @@
+CREATE TABLE patients(patient_id INT, first_name TEXT, last_name TEXT);
+CREATE TABLE doctors(doctor_id INT, first_name TEXT, last_name TEXT);
+CREATE TABLE admissions(patient_id INT, admission_date DATE, discharge_date DATE);
